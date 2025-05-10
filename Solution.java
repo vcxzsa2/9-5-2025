c39d751437cbab64a498016f08351fdee0e2678a
vcxzsa2 <kienv905@gmail.com>
2025-05-10 17:07:09 +0700

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,9 +1,5 @@
-package n9t5n2025;
-
 class Solution {
-    // Function to rotate an array by d elements in counter-clockwise direction.
     static void rotateArr(int arr[], int d) {
-        // add your code here
         d = d % arr.length;
         if(d == 0){
             return;
